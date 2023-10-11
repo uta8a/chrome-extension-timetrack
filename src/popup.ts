ac97e09fe11d988c1aecdf2a1ec4a124b92058e2
Masanori Tani <uta8a@uta8a.org>
2023-10-12 06:59:44 +0900

    fix

--- src/popup.ts
+++ src/popup.ts
@@ -1,6 +1,6 @@
 const body = document.querySelector("body");
 const $p = document.createElement("p");
-$p.innerHTML =
+$p.innerText =
 	"使い方: githubのProjectでボタンが生えるよ & ドキュメントを生成できるよ";
 if (body) {
 	body.appendChild($p);
